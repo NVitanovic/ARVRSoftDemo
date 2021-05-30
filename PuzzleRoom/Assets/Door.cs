6ecd5d34e2fc6a58dc2f839618fc56f18b3a0aa6
NVitanovic <x-team@live.com>
2021-05-30 23:28:37 +0200

    Add Door

---
+++ PuzzleRoom/Assets/Door.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator animator;
+    public Collider collider;
+    public string text;
+    private TextMesh textMesh;
+    void Start()
+    {
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        animator.SetBool("Closed", false);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        animator.SetBool("Closed", true);
+    }
+}
