4bb71d70a282a52aa485d6d458e0c58ebdc5d236
NVitanovic <x-team@live.com>
2021-06-20 02:44:00 +0200

    Add button, add hover object color, add interaction interface

---
+++ PuzzleRoom/Assets/Scripts/Button.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Button : MonoBehaviour, IInteractable
+{
+    public bool pressed = false;
+    public string text = "";
+    private Animator animator;
+    private Text textObj;
+
+    public bool Interact()
+    {
+        //Toggle the switch
+        return pressed = !pressed;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        textObj = GetComponentInChildren<Text>();
+        textObj.text = text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pressed)
+        {
+            animator.SetBool("pressed", true);
+        }
+        else
+        {
+            animator.SetBool("pressed", false);
+        }
+        
+    }
+}
